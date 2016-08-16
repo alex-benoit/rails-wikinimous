21e7dfd8df919a1da2c2bfd6a88034e5c3ba9dc5
Alex Benoit <alexander.benoit@outlook.com>
2016-08-16 16:02:02 +0100

    done controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,39 @@
 class ArticlesController < ApplicationController
-  def index
-
+ def index
+    @articles = Article.all
   end
 
   def show
-
+    @article = Article.find(params[:id])
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
-
+    @article = Article.create(article_params)
+    redirect_to article_params
   end
 
   def edit
-
+    @article = Article.find(params[:id])
   end
 
   def update
-
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
+  private
+  def article_params
+    params.require(:article).permit(:title, :done)
   end
 end
